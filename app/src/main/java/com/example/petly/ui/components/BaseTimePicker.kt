26d0fef60f9283774758ea17059b5593ad3a4ee2
JoelPMrz <joelperezmartinez01@gmail.com>
2025-05-22 00:02:43 +0200

    foco y card de visita

--- app/src/main/java/com/example/petly/ui/components/BaseTimePicker.kt
+++ app/src/main/java/com/example/petly/ui/components/BaseTimePicker.kt
@@ -1,30 +1,70 @@
-package com.example.petly.ui.components
-
-import android.app.TimePickerDialog
-import android.widget.TimePicker
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.example.petly.R
 import java.time.LocalTime
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BaseTimePicker(
     initialTime: LocalTime = LocalTime.now(),
     onDismissRequest: () -> Unit,
     onTimeSelected: (LocalTime) -> Unit
 ) {
-    val context = LocalContext.current
+    val state = rememberTimePickerState(
+        initialHour = initialTime.hour,
+        initialMinute = initialTime.minute,
+        is24Hour = true
+    )
 
-    TimePickerDialog(
-        context,
-        { _: TimePicker, hour: Int, minute: Int ->
-            onTimeSelected(LocalTime.of(hour, minute))
-        },
-        initialTime.hour,
-        initialTime.minute,
-        true
-    ).apply {
-        setOnCancelListener {
-            onDismissRequest()
+    Dialog(onDismissRequest = onDismissRequest) {
+        Surface(
+            shape = MaterialTheme.shapes.medium,
+            tonalElevation = 6.dp,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(24.dp)
+                    .wrapContentWidth()
+                    .wrapContentHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Row(
+                    horizontalArrangement = Arrangement.Start,
+                    modifier = Modifier.fillMaxWidth()
+                ){
+                    Text("Selecciona el tiempo", fontWeight = FontWeight.SemiBold, fontSize = 14.sp)
+                }
+
+                Spacer(modifier = Modifier.height(20.dp))
+                TimePicker(state = state)
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Row(
+                    horizontalArrangement = Arrangement.End,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    TextButton(onClick = onDismissRequest) {
+                        Text(stringResource(R.string.form_cancel_btn))
+                    }
+                    TextButton(
+                        onClick = {
+                            onTimeSelected(LocalTime.of(state.hour, state.minute))
+                        }
+                    ) {
+                        Text(stringResource(R.string.form_confirm_btn))
+                    }
+                }
+            }
         }
-    }.show()
+    }
 }
